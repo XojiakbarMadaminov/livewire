322cd41419f5dfe69547433980558509b5e639bb
xojiakbar <x.madaminov@uzinfocom.uz>
2026-08-14 15:09:01 +0500

    d

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'hello';
 })->name('home');
+
+
+Route::get('ai', function () {
+    $response = \App\Ai\Agents\SalesCoach::make(prompt: 'salom');
+
+    return response($response);
+});
